2d794003e6ec6091b2cd0e7dccdbc942309e1eed
Md. Mosiur Rahman <palashcse2k8@gmail.com>
2023-04-06 01:36:11 +0600

    account report updated

--- lib/sblesheba/pdf-generator/generate-branch-style-account-report.dart
+++ lib/sblesheba/pdf-generator/generate-branch-style-account-report.dart
@@ -14,14 +14,15 @@ String openingDate = DateFormat.yMMMMd('en_US').format(dateTime);
 String openingTime = DateFormat.jm().format(dateTime);
 String openingDay = DateFormat('EEEE').format(dateTime);
 double width = 0;
+double defaultFontSize = 10;
 
 Future<Document> generateAccountInformationPdf() async {
   final imageLogo = MemoryImage(
       (await rootBundle.load('assets/icons/sonali-bank-banner.jpg'))
           .buffer
           .asUint8List());
-  final imageCustomer = MemoryImage(
-      (await rootBundle.load(Customer.imageLocation)).buffer.asUint8List());
+  final imageCustomer = MemoryImage((await rootBundle.load(Customer.imageLocation)).buffer.asUint8List());
+  final imageSignture = MemoryImage((await rootBundle.load(Customer.imageSignature)).buffer.asUint8List());
 
   final pdf = Document();
   const pageTheme = PageTheme(
@@ -37,16 +38,196 @@ Future<Document> generateAccountInformationPdf() async {
               // Stack(),
               getHeaderArea(imageLogo),
               getBranchInformation(),
-              getAccountInformation(),
-              SizedBox(height: 10),
+              getAccountInformation(imageCustomer),
+              // SizedBox(height: 10),
               getPersonalDetails(),
-              SizedBox(height: 10),
-              getContactDetails()
+              SizedBox(height: 5),
+              getContactDetails(),
+              SizedBox(height: 5),
+              getOtherDetails(imageCustomer),
+              // SizedBox(height: 10),
+              getNomineeDetails(),
+              SizedBox(height: 5),
+              getSignature(imageSignture),
+              SizedBox(height: 5),
+              getLowerPart()
             ]),
           );
         }),
   );
   return pdf;
+}
+
+Widget getSignature(MemoryImage imageCustomer) {
+  return Column(
+    mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+    Align(
+      alignment: Alignment.topLeft,
+      child: Text("Speciman Signature/Digital Signature (Where necessary) :"),
+    ),
+    SizedBox(height: 10),
+    Container(
+      height: 60,
+      width: 200,
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              image: MemoryImage(imageCustomer.bytes), fit: BoxFit.cover),
+          border: Border.all()),
+    ),
+  ]);
+}
+
+Widget getLowerPart() {
+  return Container(
+    child: Column(children: [
+      Container(
+        alignment: Alignment.topLeft,
+        child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+          Align(
+            alignment: Alignment.topLeft,
+            child: Text("1. Has UNSCR's check done? (Yes) (No)"),
+          ),
+          SizedBox(height: 5),
+          Align(
+            alignment: Alignment.topLeft,
+            child: Text(
+                "2. Has review of customer profile done (Existing customer)? If so, date of review .............."),
+          ),
+          SizedBox(height: 5),
+          Align(
+            alignment: Alignment.topLeft,
+            child: Text(
+                "3. What is the average range of customer transaction (Over 6/12 months)? ........."),
+          ),
+          SizedBox(height: 5),
+          Align(
+            alignment: Alignment.topLeft,
+            child: Text(
+                "4. Any other relevant field may be add here ....................................."),
+          ),
+        ]),
+      ),
+    ]),
+  );
+}
+
+Widget getNomineeDetails() {
+  return Container(
+    alignment: Alignment.topLeft,
+    child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+      Text(
+        "Nominee Details:",
+        style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+      ),
+      Table(border: TableBorder.all(width: 1), columnWidths: {
+        0: const FixedColumnWidth(50),
+        1: const FixedColumnWidth(100),
+        2: const FixedColumnWidth(60),
+        3: const FixedColumnWidth(80),
+        4: const FixedColumnWidth(100),
+        5: const FixedColumnWidth(60),
+      }, children: [
+        TableRow(
+          decoration: BoxDecoration(
+              border: Border.all(width: 1, color: PdfColors.black)),
+          children: [
+
+            getCustomBoldText("Sl.", defaultFontSize),
+            getCustomBoldText("Nominee Name", defaultFontSize),
+            getCustomBoldText("Share %", defaultFontSize),
+            getCustomBoldText("Date Of Birth", defaultFontSize),
+            getCustomBoldText("NID/BRC", defaultFontSize),
+            getCustomBoldText("Relation", defaultFontSize),
+          ],
+        ),
+        TableRow(
+          decoration: BoxDecoration(
+              border: Border.all(width: 1, color: PdfColors.black)),
+          children: [
+            getCustomText("1.", defaultFontSize),
+            getCustomText(Customer.nomineeName, defaultFontSize),
+            getCustomText(Customer.nomineeShare, defaultFontSize),
+            getCustomText(Customer.nomineeDateofBirth, defaultFontSize),
+            getCustomText(Customer.nomineeNidBRC, defaultFontSize),
+            getCustomText(Customer.nomineeRelation, defaultFontSize),
+          ],
+        )
+      ])
+    ]),
+  );
+}
+
+Widget getOtherDetails(MemoryImage image) {
+  return Container(
+      alignment: Alignment.topLeft,
+
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            Text("Other Details:",
+                style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold)),
+            SizedBox(height: 5),
+            Table(
+                tableWidth: TableWidth.min,
+                border: const TableBorder(
+                  // left: BorderSide(color: PdfColors.black, width: 1),
+                ),
+                children: [
+                  addAccountInfoTableRow("1. Nid/BRC ", Customer.nid, defaultFontSize),
+                  addAccountInfoTableRow("2. Date of Birth ", Customer.dateOfBirth, defaultFontSize),
+                  addAccountInfoTableRow("3. TIN ", Customer.tin, defaultFontSize),
+                  addAccountInfoTableRow("4.  Gender (M/F/T)", Customer.gender, defaultFontSize),
+                  addAccountInfoTableRow("5.  Profession", Customer.profession, defaultFontSize),
+                  addAccountInfoTableRow(
+                      "6. Sopuse Name (If Applicable) ", Customer.spouseName, defaultFontSize),
+                ])
+          ]),
+          Container(
+              child: Row(
+                children: [
+                  Column(
+                      children: [
+                        Text("Nominee Photo", style: const TextStyle(fontSize: 10)),
+                        Container(
+                            height: 80,
+                            width: 150,
+                            child: Image(MemoryImage(image.bytes))
+                        ),
+                        SizedBox(width: 10)
+                      ]
+                  ),
+                  SizedBox(width: 30)
+                ]
+              )
+          )
+        ]
+      )
+  );
+}
+
+Widget getCustomBoldText(String text, double defaultFontSize ) {
+  return Padding(
+    padding: EdgeInsets.all(5),
+    child: Text(
+      text,
+      textAlign: TextAlign.center,
+      style: TextStyle(fontWeight: FontWeight.bold, fontSize: defaultFontSize),
+    ),
+  );
+}
+
+Widget getCustomText(String text, double defaultFontSize ) {
+  return Padding(
+    padding: EdgeInsets.all(4),
+    child: Text(
+      text,
+      textAlign: TextAlign.center,
+      style: TextStyle(fontWeight: FontWeight.normal, fontSize: defaultFontSize),
+    ),
+  );
 }
 
 Widget getPersonalDetails() {
@@ -65,38 +246,22 @@ Widget getPersonalDetails() {
       }, children: [
         TableRow(
           decoration: BoxDecoration(
-              border: Border.all(width: 1, color: PdfColors.black)),
+              border: Border.all(width: 1, color: PdfColors.black, style: BorderStyle.none)),
           children: [
-            Text(
-              "Client No.",
-              textAlign: TextAlign.center,
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            Text(
-              "Name",
-              textAlign: TextAlign.center,
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            Text(
-              "Father's Name",
-              textAlign: TextAlign.center,
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            Text(
-              "Mother's Name",
-              textAlign: TextAlign.center,
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
+            getCustomBoldText("Client No.", defaultFontSize),
+            getCustomBoldText("Name", defaultFontSize),
+            getCustomBoldText("Father's Name", defaultFontSize),
+            getCustomBoldText("Mother's Name", defaultFontSize),
           ],
         ),
         TableRow(
           decoration: BoxDecoration(
               border: Border.all(width: 1, color: PdfColors.black)),
           children: [
-            Text("12345", textAlign: TextAlign.center),
-            Text(Customer.name, textAlign: TextAlign.center),
-            Text(Customer.fathersName, textAlign: TextAlign.center),
-            Text(Customer.mothersName, textAlign: TextAlign.center),
+            getCustomText("12345", defaultFontSize),
+            getCustomText(Customer.name.toUpperCase(), defaultFontSize),
+            getCustomText(Customer.fathersName.toUpperCase(), defaultFontSize),
+            getCustomText(Customer.mothersName.toUpperCase(), defaultFontSize),
           ],
         )
       ])
@@ -115,46 +280,27 @@ Widget getContactDetails() {
       Table(border: TableBorder.all(width: 1), columnWidths: {
         0: const FixedColumnWidth(100),
         1: const FixedColumnWidth(100),
-        2: const FixedColumnWidth(100),
-        3: const FixedColumnWidth(100),
+        2: const FixedColumnWidth(80),
+        3: const FixedColumnWidth(120),
       }, children: [
         TableRow(
           decoration: BoxDecoration(
               border: Border.all(width: 1, color: PdfColors.black)),
           children: [
-            Text(
-              "Present Address",
-              textAlign: TextAlign.center,
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            Text(
-              "Permanent Address",
-              style: TextStyle(fontWeight: FontWeight.bold),
-              textAlign: TextAlign.center,
-            ),
-            Text(
-              "Mobile/Telephone",
-              textAlign: TextAlign.center,
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            Text(
-              "Email",
-              textAlign: TextAlign.center,
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
+            getCustomBoldText("Present Address", defaultFontSize),
+            getCustomBoldText("Permanent Address", defaultFontSize),
+            getCustomBoldText("Mobile", defaultFontSize),
+            getCustomBoldText("Email", defaultFontSize),
           ],
         ),
         TableRow(
           decoration: BoxDecoration(
               border: Border.all(width: 1, color: PdfColors.black)),
           children: [
-            Text(Customer.presentAddress, textAlign: TextAlign.center),
-            Text(Customer.permanentAddress, textAlign: TextAlign.center),
-            Align(
-              alignment: Alignment.center,
-              child: Text(Customer.mobileNumber, textAlign: TextAlign.center),
-            ),
-            Text(Customer.email, textAlign: TextAlign.center),
+            getCustomText(Customer.presentAddress, defaultFontSize),
+            getCustomText(Customer.permanentAddress, defaultFontSize),
+            getCustomText(Customer.mobileNumber, defaultFontSize),
+            getCustomText(Customer.email, defaultFontSize),
           ],
         )
       ])
@@ -162,41 +308,71 @@ Widget getContactDetails() {
   );
 }
 
-Widget getAccountInformation() {
+Widget getAccountInformation(MemoryImage image) {
   return Container(
       alignment: Alignment.topLeft,
-      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-        Text("Account Information:",
-            style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold)),
-        SizedBox(height: 10),
-        Table(
-            tableWidth: TableWidth.min,
-            border: const TableBorder(
-                // left: BorderSide(color: PdfColors.black, width: 1),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+              crossAxisAlignment: CrossAxisAlignment.start, children: [
+            Text("Account Information:",
+                style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold)),
+            SizedBox(height: 6),
+            Table(
+                tableWidth: TableWidth.min,
+                border: const TableBorder(
+                  // left: BorderSide(color: PdfColors.black, width: 1),
                 ),
-            children: [
-              addAccountInfoTableRow(
-                  "1. Account Number ", Customer.accountNumber),
-              addAccountInfoTableRow("2. Account Title ", Customer.name),
-              addAccountInfoTableRow("3. Product Name ", "Savings Deposit"),
-              addAccountInfoTableRow("4. Product Code ", "1000"),
-            ])
-      ]));
+                children: [
+                  addAccountInfoTableRow("1. Account Number ", Customer.accountNumber, defaultFontSize),
+                  addAccountInfoTableRow("2. Account Title ", Customer.name.toUpperCase(), defaultFontSize),
+                  addAccountInfoTableRow("3. Product Name ", "Savings Deposit", defaultFontSize),
+                  addAccountInfoTableRow("4. Product Code ", "1000", defaultFontSize),
+                ])
+          ]),
+          Container(
+            child:
+            Row(
+              children: [
+                Column(
+                    children: [
+                      Text("Customer Photo", style: TextStyle(fontSize: 10)),
+                      Container(
+                          height: 80,
+                          width: 150,
+                          child: Image(MemoryImage(image.bytes))
+                      ),
+                      SizedBox(width: 10)
+                    ]
+                ),
+                SizedBox(width: 30)
+              ]
+            )
+          )
+        ]
+      )
+
+  );
 }
 
-TableRow addAccountInfoTableRow(String option, String value) {
+TableRow addAccountInfoTableRow(String option, String value, double defaultfontSize) {
   return TableRow(children: [
     Container(
       height: 20,
       child: Text(option,
           textAlign: TextAlign.left,
-          style: TextStyle(fontWeight: FontWeight.normal)),
+          style: TextStyle(fontWeight: FontWeight.normal,
+            fontSize: defaultfontSize,
+          )),
     ),
     Container(
       height: 20,
       child: Text(': $value',
           textAlign: TextAlign.left,
-          style: TextStyle(fontWeight: FontWeight.normal)),
+          style: TextStyle(fontWeight: FontWeight.normal,
+            fontSize: defaultfontSize,
+          )),
     ),
   ]);
 }
@@ -211,7 +387,8 @@ Widget getBranchInformation() {
 }
 
 Row getHeaderArea(MemoryImage imageLogo) {
-  return Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+  return Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
     Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -229,14 +406,16 @@ Row getHeaderArea(MemoryImage imageLogo) {
           ),
           Text("Generated By",
               style: const TextStyle(
-                fontSize: 10,
+                fontSize: 8,
               )),
           Text(
-            "Information Technology Division (Business IT)",
+            "Information Technology Division",
+            style: const TextStyle(fontSize: 10)
           ),
-          Text(
-            "Head Office",
-          ),
+          // Text(
+          //   "Head Office",
+          //     style: const TextStyle(fontSize: 10)
+          // ),
         ]),
     Column(
         mainAxisAlignment: MainAxisAlignment.center,
