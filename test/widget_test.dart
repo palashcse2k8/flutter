79346e949e38ffad915e87e8821f1cd8646d99c7
Md. Mosiur Rahman <palashcse2k8@gmail.com>
2023-04-12 15:11:25 +0600

    reporting

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,11 +8,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutterbascis/main.dart';
+import 'package:flutterbascis/sblesheba/provider/app_language_provider.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(
+        MyApp(appLanguage: AppLanguageProvider() ));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
